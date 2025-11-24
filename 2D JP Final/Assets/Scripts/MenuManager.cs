c7bc59a591c2a7c8fc698626c683783072959c7c
John Torres <torj16@protonmail.com>
2025-11-24 14:10:41 -0500

    Base menu functionality implemented

---
+++ 2D JP Final/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button NewGameButton;
+    public Button ExitGameButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        NewGameButton = GameObject.Find("New Game Button").GetComponent<Button>();
+        NewGameButton.onClick.AddListener(NewGame);
+        ExitGameButton = GameObject.Find("Exit Game Button").GetComponent<Button>();
+        ExitGameButton.onClick.AddListener(ExitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NewGame() { SceneManager.LoadScene("StartArea"); }
+
+    private void ExitGame()
+    {
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+
+}
